bde0452c37396dd2054ca3e072c215a6f37f0543
Mike Coomber <mikecoomber99@gmail.com>
2021-04-19 17:33:05 +0100

    Added validation to rhythm names

--- app/src/main/java/com/example/rhythmnotator/fragments/SaveDialogFragment.kt
+++ app/src/main/java/com/example/rhythmnotator/fragments/SaveDialogFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.example.rhythmnotator.R
 import com.example.rhythmnotator.utils.ExtendedContext
 import com.example.rhythmnotator.databinding.DialogSaveBinding
 
@@ -33,20 +34,36 @@ class SaveDialogFragment : DialogFragment() {
 
             val context = activity!!.applicationContext as ExtendedContext
 
-            val fileName = binding.fileNameInput.text.toString()
-            val bpm = context.bpm.toString()
-            val beatsInABar = context.beatsInABar.toString()
-            val rhythm = context.currentNoteData
+            val fileName = binding.fileNameInput.editText?.text.toString()
 
-            val fileContents = "$bpm;$beatsInABar;$rhythm"
-            Log.d(logTag, "file contents: $fileContents")
-            context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
-                it.write(fileContents.toByteArray())
+            when {
+                context.fileList().contains(fileName) -> {
+                    binding.fileNameInput.isErrorEnabled = true
+                    binding.fileNameInput.error = getString(R.string.rhytm_used_error)
+
+                }
+                fileName.contains('/') -> {
+                    binding.fileNameInput.isErrorEnabled = true
+                    binding.fileNameInput.error = getString(R.string.reserved_characters_error)
+
+                }
+                else -> {
+                    val bpm = context.bpm.toString()
+                    val beatsInABar = context.beatsInABar.toString()
+                    val rhythm = context.currentNoteData
+
+                    val fileContents = "$bpm;$beatsInABar;$rhythm"
+                    Log.d(logTag, "file contents: $fileContents")
+                    context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
+                        it.write(fileContents.toByteArray())
+                    }
+
+                    Log.d(logTag, "files: ${context.fileList().joinToString(",")}")
+
+                    dismiss()
+                }
             }
 
-            Log.d(logTag, "files: ${context.fileList().joinToString(",")}")
-
-            dismiss()
         }
 
         binding.cancelButton.setOnClickListener {
